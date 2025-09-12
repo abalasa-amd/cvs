f38f81ba0abc789ead570c78e1cfbc947724513a
Urvashi Tiwari <urtiwari@amd.com>
2025-09-11 20:41:32 -0400

    Adding Header

--- tests/rccl/rccl_multinode_cvs.py
+++ tests/rccl/rccl_multinode_cvs.py
@@ -1,3 +1,10 @@
+'''
+Copyright 2025 Advanced Micro Devices, Inc.
+All rights reserved. This notice is intended as a precaution against inadvertent publication and does not imply publication or any waiver of confidentiality.
+The year included in the foregoing notice is the year of creation of the work.
+All code contained here is Property of Advanced Micro Devices, Inc.
+'''
+
 import pytest
 
 import re
