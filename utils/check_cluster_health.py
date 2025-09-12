cfe760879ec82860423375f49a1de492bfd45a6f
Urvashi Tiwari <urtiwari@amd.com>
2025-09-11 20:43:15 -0400

    Adding Header

--- utils/check_cluster_health.py
+++ utils/check_cluster_health.py
@@ -1,4 +1,9 @@
-
+'''
+Copyright 2025 Advanced Micro Devices, Inc.
+All rights reserved. This notice is intended as a precaution against inadvertent publication and does not imply publication or any waiver of confidentiality.
+The year included in the foregoing notice is the year of creation of the work.
+All code contained here is Property of Advanced Micro Devices, Inc.
+'''
 
 
 import os
